d671e29cd3e8266321a8920c346aeeee5210e054
Plamask <115168286+Plamask@users.noreply.github.com>
2022-10-22 14:06:03 +0200

    Update Connessione.java

--- ConnessioneDb/src/main/java/it/giuseppe/Connessione.java
+++ ConnessioneDb/src/main/java/it/giuseppe/Connessione.java
@@ -10,16 +10,14 @@ import lombok.ToString;
 @ToString
 public class Connessione {
 
-	static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
-	  static final String DB_URL = "jdbc:mysql://localhost/gestione_surgelati";
-	  static final String USER = "giuseppe";
-	  static final String PASS = "peppe";
-	public Connessione() {
+	
+	public Connessione(String JDBC_DRIVER, String DB_URL, String USER, String PASS, String sql) {
 		
 		
 
 		
-		    Connection conn = null;
+		 Connection conn = null;
+		    Statement stmt = null;
 		    
 		    try {
 		      Class.forName(JDBC_DRIVER);
@@ -35,14 +33,31 @@ public class Connessione {
 		        
 		        if (conn != null) {
 		        	System.out.println("Connessione al db effettuata");
-		        }
+		        	System.out.println("Premi 1 per fare una select , 2 per insert, 3 per update, 4 per delete");
+		        	scelta = input.nextInt();
+		        	if(scelta == 1) {
+		        		stmt = conn.createStatement();
+		  		     
+		  		      ResultSet rs = stmt.executeQuery(sql);
+		  		      while (rs.next()) {
+		  		        
+		  		        String nome = rs.getString("nome_cliente");
+		  		        String indirizzo = rs.getString("indirizzo_cliente");
+
+		  		        System.out.print("nome: " + nome);
+		  		        System.out.println(", indirizzo: " + indirizzo);
+		  		       
+		  		      }
+		  		      rs.close();
+		  		      stmt.close();
+		        		
+		        	}
+				 }
 		        conn.close();
 		      } catch (SQLException se) {
 		        se.printStackTrace();
 		      }
 		      
-		    }
-		  
-	}
+		    }  
 	
 }
